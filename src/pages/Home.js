4a21728c47bb4c757fc6e206fdda0e7d5241e403
Creadoc <jcoll06t@gmail.com>
2022-06-15 14:45:40 -0500

    a few changes have been done to home and projects files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>
+        <h1>Welcome! I'm Jimmy.</h1>
+        <h3>Here is some info about me: </h3>
+        <p>
+          I don't even know where to start, but here goes. I obtained my
+          Bachelor's Degree from
+          <a href="https://uafs.edu/" rel="noreferrer" target="_blank">
+            UAFS
+          </a>
+          in May of 2022. I have been working as an IT Co-Op for Rheem
+          Manufacturing since November 2019. I am a father to 5 wonderful
+          children. I am passionate about learning, especially when it comes to
+          computer science and programming.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div>
+      <div>Projects</div>
+      <h2>
+        Here is a list of my projects, You'll find them on my Github account.
+        Keep checking back for an update as I am usually working on something!
+      </h2>
+      <a href="https://github.com/Creadoc" rel="noreferrer" target="_blank">
+        My Github Profile
+      </a>
+    </div>
+  );
 }
 
 export default Projects;
