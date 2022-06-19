b55708f335e2eff794aa83fa116329ad7661829d
jcolli04 <jcolli04@g.uafs.edu>
2022-06-19 17:12:45 -0500

    more edits to experience

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -24,7 +24,134 @@ function Experience() {
               www.rheem.com
             </a>
           </div>
-          <div></div>
+          <div>Start: 11/19 End: N/A</div>
+        </div>
+        <div className="subCont">
+          <ul>
+            <li>Imaging Service/New device setup</li>
+            <li>Hardware & Software Troubleshooting and Repair</li>
+            <li>Helpdesk Support</li>
+            <li>Windows Server AD Management</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="mainCont">
+        Fort Smith EMS
+        <div className="infoCont">
+          <div>
+            Address: 3417 Duke Ave <br /> Fort Smith <br /> AR, 72908
+          </div>
+          <div>Phone: (479) 783-1078</div>
+          <div>
+            Website:
+            <a
+              href="http://www.fortsmithems.org"
+              rel="noreferrer"
+              target="_blank"
+              className="anchorColor"
+            >
+              www.fortsmithems.org
+            </a>
+          </div>
+          <div>Start: 04/16 End: 01/2022, but still on call there</div>
+        </div>
+        <div className="subCont">
+          <ul>
+            <li> Certified ACLS, PALS, CPR, Pre-Hospital trauma</li>
+            <li>Attend all emergency call-outs</li>
+            <li> Attend to both emergent and non-emergent patients</li>
+            <li>
+              Paramedic level function, to include all duties and scope within
+              the State of Arkansas
+            </li>
+            <li>
+              Operated in both leadership and subordinate role on my unit as
+              necessary
+            </li>
+            <li>Manage and assist in both large and small scale events</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="mainCont">
+        Choctaw Nation
+        <div className="infoCont">
+          <div>
+            Address: 3400 Choctaw Rd., <br />
+            Pocola, Oklahoma <br /> 74902
+          </div>
+          <div>Phone: (918) 436-7761</div>
+          <div>
+            Website:
+            <a
+              href="http://www.choctawcasinos.com"
+              rel="noreferrer"
+              target="_blank"
+              className="anchorColor"
+            >
+              www.choctawcasinos.com
+            </a>
+          </div>
+          <div>Start: Sep 2010 & Nov 2015 End: Sep 2012 & Apr 2016</div>
+        </div>
+        <div className="subCont">
+          <ul>
+            <li> Armed security officer for Choctaw Nation </li>
+            <li> Certified Bike patrol officer</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="mainCont">
+        Franklin County EMS
+        <div className="infoCont">
+          <div>
+            Address: , <br />
+            Ozark, Arkansas <br /> 72904
+          </div>
+          <div>Phone: (479) 646-4311</div>
+          <div>
+            Website:
+            <a
+              href="http://www.rheem.com"
+              rel="noreferrer"
+              target="_blank"
+              className="anchorColor"
+            >
+              www.rheem.com
+            </a>
+          </div>
+          <div>Start: 11/19 End: N/A</div>
+        </div>
+        <div className="subCont">
+          <ul>
+            <li>Imaging Service/New device setup</li>
+            <li>Hardware & Software Troubleshooting and Repair</li>
+            <li>Helpdesk Support</li>
+            <li>Windows Server AD Management</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="mainCont">
+        AR Army National Guard
+        <div className="infoCont">
+          <div>
+            Address: Arkansas <br /> NA
+          </div>
+          <div>Phone: NA</div>
+          <div>
+            Website:
+            <a
+              href="http://www.rheem.com"
+              rel="noreferrer"
+              target="_blank"
+              className="anchorColor"
+            >
+              www.rheem.com
+            </a>
+          </div>
           <div>Start: 11/19 End: N/A</div>
         </div>
         <div className="subCont">
